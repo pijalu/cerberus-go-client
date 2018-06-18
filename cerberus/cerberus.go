5ca65c91a77692ff786fb45a144a0a88d942aa10
Pierre Poissinger <pierre.poissinger@nike.com>
2018-06-18 15:23:40 +0200

    Allow body/content type to be specified

--- cerberus/cerberus.go
+++ cerberus/cerberus.go
@@ -110,9 +110,8 @@ func (c *Client) SecureFile() *SecureFile {
 // This likely means that there is some sort of server error that is occurring
 var ErrorBodyNotReturned = fmt.Errorf("No error body returned from server")
 
-// DoRequest is used to perform an HTTP request with the given method and path
-// This method is what is called by other parts of the client and is exposed for advanced usage
-func (c *Client) DoRequest(method, path string, params map[string]string, data interface{}) (*http.Response, error) {
+// DoRequestWithBody executes a request with provided body
+func (c *Client) DoRequestWithBody(method, path string, params map[string]string, contentType string, body io.Reader) (*http.Response, error) {
 	// Get a copy of the base URL and add the path
 	var baseURL = *c.CerberusURL
 	baseURL.Path = path
@@ -124,18 +123,8 @@ func (c *Client) DoRequest(method, path string, params map[string]string, data i
 	baseURL.RawQuery = p.Encode()
 	var req *http.Request
 	var err error
-	if data == nil {
-		req, err = http.NewRequest(method, baseURL.String(), nil)
-	} else {
-		// Encode the body to send in the request if one was given
-		body := &bytes.Buffer{}
-		err := json.NewEncoder(body).Encode(data)
-		if err != nil {
-			return nil, err
-		}
-		req, err = http.NewRequest(method, baseURL.String(), body)
-	}
 
+	req, err = http.NewRequest(method, baseURL.String(), body)
 	if err != nil {
 		return nil, err
 	}
@@ -144,6 +133,12 @@ func (c *Client) DoRequest(method, path string, params map[string]string, data i
 		return nil, headerErr
 	}
 	req.Header = headers
+
+	// Add content type if present
+	if contentType != "" {
+		req.Header.Set("Content-Type", contentType)
+	}
+
 	resp, respErr := c.httpClient.Do(req)
 	if respErr != nil {
 		return nil, respErr
@@ -162,6 +157,24 @@ func (c *Client) DoRequest(method, path string, params map[string]string, data i
 		c.vaultClient.SetToken(tok)
 	}
 	return resp, nil
+}
+
+// DoRequest is used to perform an HTTP request with the given method and path
+// This method is what is called by other parts of the client and is exposed for advanced usage
+func (c *Client) DoRequest(method, path string, params map[string]string, data interface{}) (*http.Response, error) {
+	var body io.ReadWriter
+	var contentType string
+
+	if data != nil {
+		body = &bytes.Buffer{}
+		contentType = "application/json"
+		err := json.NewEncoder(body).Encode(data)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return c.DoRequestWithBody(method, path, params, contentType, body)
 }
 
 // parseResponse marshals the given body into the given interface. It should be used just like
